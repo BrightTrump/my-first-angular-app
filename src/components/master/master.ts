68a4486b117946480499315edbcbbaeb1a8327ef
Bright Munemu <brightmunemu@gmail.com>
2025-07-05 11:44:05 +0100

    create master.ts

---
+++ src/components/master/master.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Designation } from '../designation/designation';
+import { Roles } from '../roles/roles';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-master',
+  imports: [Designation, Roles, CommonModule],
+  templateUrl: './master.html',
+  styleUrl: './master.scss',
+})
+export class Master {
+  currentComponent: string = 'Roles';
+  changeTab(tabName: string) {
+    this.currentComponent = tabName;
+  }
+}
