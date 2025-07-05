a0fd2f667a34a050fc1997fc690a2dcada1e4ec5
Bright Munemu <brightmunemu@gmail.com>
2025-07-05 11:33:49 +0100

    create angular app

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Master } from '../components/master/master';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Master],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected title = 'my-angular-app';
+}
